60619f891c6a1c89421677ca16e733773e9c81db
saikrishnagalla38-ux <saikrishnagalla38@gmail.com>
2026-07-22 12:50:56 +0530

    let keyword based variable

--- SW_CSE_B1.4_INTERSHIP-/day 8/p4.js
+++ SW_CSE_B1.4_INTERSHIP-/day 8/p4.js
@@ -9,3 +9,15 @@ if (true) {
 }
 // var in JS is not block-scoped
 console.log(message);
+
+// ****************************************************
+// let keyword based variable
+let score = 100;
+//let score = 100; //SyntaxError: Identifier 'score' has already been declared
+score = 160; // re-assignment is valid
+console.log(score)
+
+if(true){
+    let secret = "Password@123"; // let in JS is block-scoped
+}
+console.log(secret); //ReferenceError: secret is not defined
