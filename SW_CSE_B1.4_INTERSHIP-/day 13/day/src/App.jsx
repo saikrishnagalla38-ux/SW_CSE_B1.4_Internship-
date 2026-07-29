215fa7bc111080782646c44c80cf34195a0cd5ce
saikrishnagalla38-ux <saikrishnagalla38@gmail.com>
2026-07-29 15:48:49 +0530

    EventHandling ex3

--- SW_CSE_B1.4_INTERSHIP-/day 13/day/src/App.jsx
+++ SW_CSE_B1.4_INTERSHIP-/day 13/day/src/App.jsx
@@ -12,6 +12,7 @@ import NameInput from "./components/StateThree";
 
 import ClickDemo from "./components/EventHandling";
 import FormDemo from "./components/EventHandlingTwo";
+import ActionButtons from "./components/EventHandlingThree";
 
 const name = "Amay";
 const isLoggedIn = false;
@@ -54,6 +55,7 @@ function App() {
 
       <ClickDemo/>
       <FormDemo/>
+      <ActionButtons/>
     </>
     
   )
