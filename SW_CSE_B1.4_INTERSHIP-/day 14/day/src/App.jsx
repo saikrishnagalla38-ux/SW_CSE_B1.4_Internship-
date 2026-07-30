f3944bfc2bd54a9d666bf252a46421365855ea4d
saikrishnagalla38-ux <saikrishnagalla38@gmail.com>
2026-07-30 15:35:02 +0530

    Form ex:1

--- SW_CSE_B1.4_INTERSHIP-/day 14/day/src/App.jsx
+++ SW_CSE_B1.4_INTERSHIP-/day 14/day/src/App.jsx
@@ -5,6 +5,8 @@ import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
+import SimpleNameForm from "./components/FormOne";
+
 function App() {
   
   return (
@@ -15,7 +17,8 @@ function App() {
 
       <CounterHook/> */}
       {/* <UserListHook/> */}
-      <ThemeProviderDemo />
+      {/* <ThemeProviderDemo /> */}
+      <SimpleNameForm />
     </>
   )
 }
