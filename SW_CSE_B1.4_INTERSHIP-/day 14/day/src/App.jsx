208f58d5a55d6c848c463339ad159670188de035
saikrishnagalla38-ux <saikrishnagalla38@gmail.com>
2026-07-30 15:07:12 +0530

    Hooks ex3: useContext

--- SW_CSE_B1.4_INTERSHIP-/day 14/day/src/App.jsx
+++ SW_CSE_B1.4_INTERSHIP-/day 14/day/src/App.jsx
@@ -4,7 +4,7 @@ import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
-
+import ThemeProviderDemo from "./components/HookThree";
 function App() {
   
   return (
@@ -14,7 +14,8 @@ function App() {
       <ProductListDemo />
 
       <CounterHook/> */}
-      <UserListHook/>
+      {/* <UserListHook/> */}
+      <ThemeProviderDemo />
     </>
   )
 }
